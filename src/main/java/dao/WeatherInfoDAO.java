275bbe12277d88b04cfda21576a890929b80bc63
papertigerv5 <papertigerv5@163.com>
2013-01-24 00:22:18 +0800

    Convert project to maven manage. And finish the last part to store the information to the db.

---
+++ src/main/java/dao/WeatherInfoDAO.java
@@ -0,0 +1,17 @@
+package dao;
+
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yifan
+ * Date: 13-1-23
+ * Time: 下午10:20
+ * To change this template use File | Settings | File Templates.
+ */
+public class WeatherInfoDAO {
+
+   public static void main(String[] args) {
+
+   }
+
+}
